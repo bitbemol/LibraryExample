ab899a80057c58d71d63754e4edd03eb3e3a2ae8
Bemol <test@local.com>
2023-10-27 21:40:46 -0600

    Added another public

--- Sources/LibraryExample/LibraryExample.swift
+++ Sources/LibraryExample/LibraryExample.swift
@@ -2,7 +2,7 @@
 // https://docs.swift.org/swift-book
 
 public struct LibraryExample {
-    func sayHi(to name: String) -> String {
+    public func sayHi(to name: String) -> String {
         "Hi, \(name)"
     }
 }
