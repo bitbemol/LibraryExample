a8d77946bcbf8dc5f409f0b80de223ff415d1aec
Bemol <test@local.com>
2023-10-27 21:39:05 -0600

    Added public to be able to access the struct outside the package

--- Sources/LibraryExample/LibraryExample.swift
+++ Sources/LibraryExample/LibraryExample.swift
@@ -1,7 +1,7 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-struct LibraryExample {
+public struct LibraryExample {
     func sayHi(to name: String) -> String {
         "Hi, \(name)"
     }
