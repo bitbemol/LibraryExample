7ee688cbb58c8111a354b0f2218fa41aaeaf2638
Bemol <test@local.com>
2023-10-27 21:32:20 -0600

    Added simple example

--- Sources/LibraryExample/LibraryExample.swift
+++ Sources/LibraryExample/LibraryExample.swift
@@ -1,2 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+struct LibraryExample {
+    func sayHi(to name: String) -> String {
+        "Hi, \(name)"
+    }
+}
--- Tests/LibraryExampleTests/LibraryExampleTests.swift
+++ Tests/LibraryExampleTests/LibraryExampleTests.swift
@@ -2,11 +2,12 @@ import XCTest
 @testable import LibraryExample
 
 final class LibraryExampleTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    
+    var sut: LibraryExample = LibraryExample()
+    
+    func testSayHi() {
+        let input = "Alex"
+        let expected = "Hi, Alex"
+        XCTAssertEqual(sut.sayHi(to: input), expected)
     }
 }
